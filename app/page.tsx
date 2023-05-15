dcde9bbef5310aff01057f6ab8fcf4893f694c58
akilakavinda99 <akilakavinda909@gmail.com>
2023-05-15 10:38:45 +0530

    testingd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  // const unUSed = 0;
+  const unUSed = 0;
   // console.log(unUSed);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
